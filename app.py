63bca38919a53f3d2953fa7e3a8a7759398e0ef0
Lencho <lencholemessa@gmail.com>
2026-08-09 05:15:07 -0700

    Update app.py

--- app.py
+++ app.py
@@ -230,7 +230,7 @@ with st.sidebar:
 # 5. Hero Header
 st.markdown("""
 <div class="hero-container">
-    <div class="hero-title">🎧 <span class="lencho-highlight">LENCHOS</span> VOICE LAB</div>
+    <div class="hero-title">🎧 <span class="lencho-highlight">Lencho x Latera</span> Audio Studio</div>
     <div class="hero-subtitle">Studio-Grade Text-to-Speech Engine Created by <span class="lencho-highlight">Lencho</span> Lemessa For <span class="lencho-highlight">Latera</span> Lemessa </div>
 </div>
 """, unsafe_allow_html=True)
